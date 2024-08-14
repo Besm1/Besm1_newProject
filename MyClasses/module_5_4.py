3a034bf5084e259c949254a8a172d98ebf45d99b
Besm1 <besm1@yandex.ru>
2024-08-14 22:39:46 +0300

    Создали module_5_4

--- MyClasses/module_5_4.py
+++ MyClasses/module_5_4.py
@@ -1,6 +1,12 @@
 class House():
-    name = ''
-    number_of_floors = 0
+    # name = ''
+    # number_of_floors = 0
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.args = args
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
 
     def __init__(self, name, floors):
         self.name, self.number_of_floors = name, floors
@@ -70,26 +76,20 @@ class House():
         return self.__sub__(value)
 
 
+    def __del__(self):
+        print('{} разрушен, но он останется в истории.'.format(self.name))
+
+
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
 h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
 
-print(h1)
-print(h2)
+# Удаление объектов
+del h2
+del h3
 
-print(h1 == h2) # __eq__
+print(House.houses_history)
 
-h1 = h1 + 10 # __add__
-print(h1)
-print(h1 == h2)
-
-h1 += 10 # __iadd__
-print(h1)
-
-h2 = 10 + h2 # __radd__
-print(h2)
-
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
