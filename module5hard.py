8372e320dd16b74846b7f4c227ab5540b611118c
Besm1 <besm1@yandex.ru>
2024-08-18 00:30:06 +0300

    Теперь и watch_video добавили!

--- module5hard.py
+++ module5hard.py
@@ -1,4 +1,3 @@
-import hashlib
 from time import sleep
 import tkinter as tk
 
@@ -20,6 +19,7 @@ class User:
     def find(self, username):
         return self.nickname.find(username)
 
+
 class Video:
     def __init__(self, title, duration, adult_mode=False):
         self.title = title
@@ -28,71 +28,74 @@ class Video:
         self.time_now: int = 0
 
     def __str__(self):
-        return f'{self.title}, {self.duration} сек.'  + (' (18+)' if self.adult_mode else '')
+        return f'{self.title}, {self.duration} сек.' + (' (18+)' if self.adult_mode else '')
+
 
 class UrTube:
-    def __init__(self, users = [], videos = []):
-        self.users = users
-        self.videos = videos
+    def __init__(self):
+        self.users = []
+        self.videos = []
         self.current_user = None
 
     def add(self, *vidosiki):
         if all([isinstance(o_, Video) for o_ in vidosiki]):
             for v_ in [new_v for new_v in vidosiki if new_v.title not in [exist_v.title for exist_v in self.videos]]:
-                # print(f'Добавляем на сайт фильм "{v_.title}"')
+                print(f'* Добавляем на сайт фильм "{v_.title}"')
                 self.videos.append(v_)
 
-
     def get_videos(self, pattern: str):
+        print(f'* Подбираем фильмы, в названии которых есть строка "{pattern}"')
         return [str(v_) for v_ in self.videos if v_.title.lower().find(pattern.lower()) >= 0]
 
-    def register(self,username:str, password: str, age: int):
+    def register(self, username: str, password: str, age: int):
+        print(f'* Попытка регистрации пользователя {username}')
         if username not in [n_.nickname for n_ in self.users]:
             self.current_user = User(username, password, age)
             self.users.append(self.current_user)
-            print(f'Приятно познакомиться, {self.current_user.nickname}! ')
+            print(f'  Приятно познакомиться, {self.current_user.nickname}! ')
         else:
             self.current_user = None
-            print(f'Пользователь {username} уже существует')
-
+            print(f'  Пользователь {username} уже существует')
 
     def log_in(self, username, password):
+        print(f'* Попытка входа пользователя {username}.')
         user = [u_ for u_ in self.users if u_.user_name().lower() == username.lower()]
         if len(user) == 1:
-            print(f'{user[0].nickname}, это ты?')
+            print(f'  {user[0].nickname}, это ты?')
             if user[0].password == hash(password):
-                print(f'Здравствуй, {user[0].nickname}! Выбирай фильм. Приятного просмотра!')
+                print(f'  Здравствуй, {user[0].nickname}! Выбирай фильм. Приятного просмотра!')
                 self.current_user = user[0]
             else:
-                print(f'Что-то ты на себя не похож... Вспоминай пароль!')
+                print(f'  Что-то ты на себя не похож... Вспоминай пароль!')
                 self.current_user = None
         else:
-            print(f'{username}, или я тебя не знаю, или ты имя своё забыл. Вспомни имя или зарегистрируйся на UrTube.')
-
+            print(f'  {username}, или я тебя не знаю, или ты имя своё забыл. Вспомни имя или зарегистрируйся на UrTube.')
 
     def log_out(self):
-        if self.current_user != None:
-            print('А никто и не зарегистрирован!..')
+        print('* Попытка выхода')
+        if self.current_user is None:
+            print('  А никто и не входил!..')
         else:
-            print(f'До свидания, {self.current_user.nickname}! Приходи ещё!')
+            print(f'  До свидания, {self.current_user.nickname}! Приходи ещё!')
             self.current_user = None
 
-    def watch_video(self,film_name: str):
-        if self.current_user == None:
-            print('Сначала войди или зарегистрируйся на сайте!')
+    def watch_video(self, film_name: str):
+        print(f'* Пользователь {self.current_user} хочет посмотреть фильм "{film_name}".')
+        if self.current_user is None:
+            print('  Сначала войди или зарегистрируйся на сайте!')
         else:
             video = [v_ for v_ in self.videos if v_.title == film_name]
             if len(video) == 1:
-                 if not video[0].adult_mode or self.current_user.age >= 18:
-                     print(f'Смотрим фильм "{str(video[0])}"')
-                     p = Player(video[0])
-                 else:
-                     print(f'{self.current_user.nickname}, тебе рано ещё смореть такие фильмы! '
-                           + f'Приходи лет эдак через {18 - self.current_user.age}!')
+                if not video[0].adult_mode or self.current_user.age >= 18:
+                    Player(video[0])
+                else:
+                    print(f'  {self.current_user.nickname}, тебе рано ещё смореть такие фильмы! '
+                          + f'Приходи лет эдак через {18 - self.current_user.age}!')
             else:
-                print(f'Фильм "{film_name}" не найден, наверное, ещё не завезли. Попробуй позже.')
+                print(f'  Фильм "{film_name}" не найден, наверное, ещё не завезли. Попробуй позже.')
 
-class Player():
+
+class Player:
 
     def __init__(self, vidos: Video):
 
@@ -101,25 +104,24 @@ class Player():
         # window.title(vidos.title)
         # window.geometry('800x450')
         # window.resizable(False, False)
-        # text = tk.Label(window, text=f'Смотрим: {vidos.time_now} сек.')
-        # text.place(x=350,y=220)
-        # while vidos.time_now <= vidos.duration:
+        # text = tk.Entry(window, width=60)
+        # text.place(x=200, y=220)
+        # text.insert(0, f'Смотрим: {vidos.time_now} сек.')
+        # while vidos.time_now < vidos.duration:
         #     sleep(1)
         #     vidos.time_now += 1
-        #     text = tk.Label(window, text=f'Смотрим: {vidos.time_now} сек.')
-        #     text.place(x=350, y=220)
-        # text = tk.Label(window, text='Ребята, а что вы тут делаете, а? Кино-то давно кончилось!')
-        # text.place(x=350, y=220)
-        # button = tk.Button(window, text='Закрыть плеер', width=40, height=5)
+        #     text.delete(0, 'end')
+        #     text.insert(0, f'Смотрим: {vidos.time_now} сек.')
+        # text.insert(0, 'Ребята, а что вы тут делаете, а? Кино-то давно кончилось!')
         # window.mainloop()
-        print(f'Смотрим: {vidos.time_now} сек.')
+        # button = tk.Button(window, text='Закрыть плеер', width=40, height=5)
+
+        print(f'  Смотрим фильм "{str(vidos)}"')
         while vidos.time_now < vidos.duration:
             sleep(1)
             vidos.time_now += 1
-            print(f'Смотрим: {vidos.time_now} сек.')
-        print('\nРебята, а что вы тут делаете, а? Кино-то давно кончилось!\n\n')
-
-
+            print(f'  {vidos.time_now}', end = '')
+        print('  Ребята, а что вы тут делаете, а? Кино-то давно кончилось!')
 
 
 if __name__ == '__main__':
@@ -131,8 +133,8 @@ if __name__ == '__main__':
     ur.add(v1, v2)
 
     # Проверка поиска
-    print(ur.get_videos('лучший'))
-    print(ur.get_videos('ПРОГ'))
+    print(f"  {ur.get_videos('лучший')}")
+    print(f"  {ur.get_videos('ПРОГ')}")
 
     # Проверка на вход пользователя и возрастное ограничение
     ur.watch_video('Для чего девушкам парень программист?')
@@ -143,7 +145,6 @@ if __name__ == '__main__':
 
     # Проверка входа в другой аккаунт
     ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
-    print(ur.current_user)
 
     # Попытка воспроизведения несуществующего видео
     ur.log_in('urban_pythonist', 'iScX4vIJClb9YQavjAgF')
