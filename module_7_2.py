135120acd733f4ebd738b58ae3bc89c9ef841bd6
Besm1 <besm1@yandex.ru>
2024-09-02 21:49:44 +0300

    Помещаем module_7_1

---
+++ module_7_2.py
@@ -0,0 +1,23 @@
+def custom_write(filename: str, info: list):
+    file = open(filename, 'w', encoding='utf-8')
+    ret = {}
+    i = 0
+    for str_ in info:
+        i += 1
+        ret[(i, file.tell())] = str_
+        file.write(str_ + '\n')
+
+    return ret
+
+
+if __name__ == '__main__':
+    info = [
+        'Text for tell.',
+        'Используйте кодировку utf-8.',
+        'Because there are 2 languages!',
+        'Спасибо!'
+    ]
+
+    print(result)
+    for elem in result.items():
+        print(elem)
